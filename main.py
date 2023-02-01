1ad9d4c5639f2d0a3abcf648434fd50b48681c82
Maksim <grudinskij97@mail.ru>
2023-02-01 15:33:11 +0800

    Добавил домашнее задание

---
+++ main.py
@@ -0,0 +1,51 @@
+# print("Введите первое число: ")
+# a = int(input())
+# b = int(input("Введите второе число: "))
+
+# print(a, '+', b, '=', a+b)
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# a = 4.45353
+# b = 5.435345
+# print(round(a*b, 3))
+
+# a = 1>4
+# print(a)
+
+# a = 1<4 and 5>2
+# print(a)
+
+# a = 1 == 2
+# print(a)
+
+# a = 1 != 2
+# print(a)
+
+# a = 'qwe'
+# b = 'qwe'
+# print(a == b)
+
+# a = 1<3<5<10
+# print(a)
+
+# username = input('Введите имя: ')
+# if username =='Маша':
+#     print('Ура, это же Маша!')
+# elif username == 'Марина':
+#     print('Я так ждала Вас Марина!')
+# elif username == 'Ильнар':
+#     print('Ильнар - топ!')   
+# else:
+#     print('Привет, ', username) 
+
+a = 'qwerty'
+
+# print(a[2])
+for i in a:
+    print(i)
